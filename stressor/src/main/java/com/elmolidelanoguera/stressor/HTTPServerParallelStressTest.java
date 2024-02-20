21bc6bae2800032d4f789223cc896b60d40bc58c
Joan Ribalta <joanribalta@elmolidelanoguera.com>
2024-02-20 10:38:35 +0100

    Added Average of concurrent requests

--- stressor/src/main/java/com/elmolidelanoguera/stressor/HTTPServerParallelStressTest.java
+++ stressor/src/main/java/com/elmolidelanoguera/stressor/HTTPServerParallelStressTest.java
@@ -24,12 +24,12 @@ public class HTTPServerParallelStressTest
     static String BASE_URL = "http://localhost:8080/apis/characters/";
 
     static List<Integer> ids = Arrays.asList(
-            2, 14, 21, 18, 1, 27, 53, 3, 5, 77,
+            2, 14, 21, 18, 1, 27, 53, 3, 5, 77, 
             78, 79, 85, 103, 164, 177, 215, 232, 7, 274,
             294, 298, 15, 299, 329, 22, 330, 39, 339, 19,
             389, 424, 425, 381, 83, 427, 82, 430, 431, 84,
             433, 86, 663, 113, 118, 119, 152, 209, 242, 290,
-            295, 349, 359, 405, 428, 426, 429, 432, 434
+            295, 349, 359, 405, 428, 426, 429, 432, 434, 4
     );
 
     static final int CYCLE_NUMBER = 100;
@@ -96,23 +96,31 @@ public class HTTPServerParallelStressTest
         long totalTime = endTime - startTime;
 
         System.out.println("\nRESULTS --------------------------------------------");
-        System.out.println("Request per second: " + 
-        ((CYCLE_NUMBER * numRequestsPerCycle) / (totalTime))*1000);
+        
+//
+        long requestNumber = CYCLE_NUMBER * numRequestsPerCycle;
+        long requestPerSecond = (long)(CYCLE_NUMBER * numRequestsPerCycle*1000L) / totalTime;
+        double averageConcurrentRequests = (realTimeStatistics.calculateMean() * requestNumber)/ totalTime;        
+        
+        System.out.println("Request per second: " + requestPerSecond);        
+        System.out.println("Average of concurrent requests: " + averageConcurrentRequests);
         System.out.println("Total time of all requests: " + totalTime + " ms");
+        //
         System.out.println("Minimum time requests: " + minimum + " ms");
         System.out.println("Maximum time requests: " + maximum + " ms");
         System.out.println("Mean: " + 
                 String.format("%.2f", realTimeStatistics.calculateMean()) + " ms");        
         System.out.println("Standard Deviation: " + 
                 String.format("%.2f",realTimeStatistics.calculateStandardDeviation()) + " ms");
+        //
         System.out.println("Delayed Requests: " + delayedRequests);
         System.out.println("Requests with exception: " + exception);
         System.out.println("Requests with error: " + error);
 
-        System.out.println("\nSETTINGS -------------------------------------------");
+        System.out.println("\nCLIENT SETTINGS -------------------------------------------");
         System.out.println("Platform threads used: " + platformThreadsUsed);
         System.out.println("Minimum time between request: " + minimumTimeBetweenRequestsInNs/1000 + " µs");
-        System.out.println("Request Number: " + (CYCLE_NUMBER * numRequestsPerCycle));
+        System.out.println("Request Number: " + requestNumber);
 
         executor.shutdown();
     }
