daba9b5e65da5647cccc43e58e9477e774108e3c
Joan Ribalta <joanribalta@elmolidelanoguera.com>
2024-02-26 11:47:23 +0100

    structural modifications

--- stressor/README.md
+++ stressor/README.md
@@ -6,7 +6,7 @@ This Java application performs parallel stress testing on an HTTP server to meas
 
 The application sends requests to an HTTP server for all configured `ids` a specified number of times defined in `CYCLE_NUMBER` in parallel using virtual threads. These virtual threads run within a maximum of `PLATFORM_THREAD_USED` platform threads. The goal is to determine the server's maximum throughput for a specific server configuration by setting `DEFAULT_MINIMUM_TIME_BETWEEN_REQUESTS_IN_MICROSECONDS`, which represents the minimum time between requests in microseconds.
 
-The `PLATFORM_THREAD_USED` parameter limits the number of platform threads used by the stressor to prevent monopolization of the machine's resources and allows for running both the client and the server on the same machine. For example, the server could be running inside a Docker container, which allows limiting the number of CPU cores used by each container.
+The `PLATFORM_THREADS_USED` parameter limits the number of platform threads used by the stressor to prevent monopolization of the machine's resources and allows for running both the client and the server on the same machine. For example, the server could be running inside a Docker container, which allows limiting the number of CPU cores used by each container.
 
 ## Usage
 
@@ -20,11 +20,11 @@ The parameter `2000` is optional and represents the minimum number of microsecon
 ## Configuration
 
 - `CYCLE_NUMBER`: Number of request cycles to perform.
-- `PLATFORM_THREAD_USED`: Number of platform threads to use (0 for the number of CPU cores).
+- `PLATFORM_THREADS_USED`: Number of platform threads to use (0 for the number of CPU cores).
 - `DEFAULT_MINIMUM_TIME_BETWEEN_REQUESTS_IN_MICROSECONDS`: Default minimum time between requests in microseconds.
 - `MAXIMUM_TIME_ALL_THREADS`: Maximum time for all threads to complete execution.
 - `BASE_URL`: Base URL of the API endpoint.
-- `ids`: List of IDs to include in the requests.
+- `ids`: List of IDs to cycle in the requests.
 
 ## Dependencies
 
--- stressor/src/main/java/com/elmolidelanoguera/stressor/HTTPServerParallelStressTest.java
+++ stressor/src/main/java/com/elmolidelanoguera/stressor/HTTPServerParallelStressTest.java
@@ -3,138 +3,250 @@
  * License: CC BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
  * Blog Consolidando: https://diy.elmolidelanoguera.com/
  */
-
 package com.elmolidelanoguera.stressor;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.List;
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class HTTPServerParallelStressTest
 {
-    static String BASE_URL = "http://localhost:8080/apis/characters/";
 
-    static List<Integer> ids = Arrays.asList(
-            2, 14, 21, 18, 1, 27, 53, 3, 5, 77, 
-            78, 79, 85, 103, 164, 177, 215, 232, 7, 274,
-            294, 298, 15, 299, 329, 22, 330, 39, 339, 19,
-            389, 424, 425, 381, 83, 427, 82, 430, 431, 84,
-            433, 86, 663, 113, 118, 119, 152, 209, 242, 290,
-            295, 349, 359, 405, 428, 426, 429, 432, 434, 4
-    );
-
-    static final int CYCLE_NUMBER = 100;
-    static final int PLATFORM_THREAD_USED = 6; // 0 => number of cores
-    static final long DEFAULT_MINIMUM_TIME_BETWEEN_REQUESTS_IN_MICROSECONDS = 2_000; // .µs
-    static final long MAXIMUM_TIME_ALL_THREADS = 60_000; // .ms
-
-    private static final AtomicLong minimum = new AtomicLong(1000);
-    private static final AtomicLong maximum = new AtomicLong(0);
+    private static int numRequestsPerCycle = 0;
+    private static int platformThreadsUsed = 0;
+    private static long minimumConcurrentRequest = 0;
+    private static long maximumConcurrentRequest = 0;
+    private static long minimumTimeBetweenRequestsInNs = 0;
+    //
+    private static final AtomicLong concurrentRequest = new AtomicLong(0L);
+    private static final AtomicLong maximumConcurrentRequestAchieve = new AtomicLong(0L);
     private static final AtomicLong exception = new AtomicLong(0);
+    //private static final AtomicLong noIOexception = new AtomicLong(0);
     private static final AtomicLong error = new AtomicLong(0);
     private static final AtomicLong responseNumber = new AtomicLong(0);
-    private static long delayedRequests = 0;
+    private static final AtomicLong exceptionRequestRetries = new AtomicLong(0);
     private static long previousRequestTimeInNs = 0;
     private static final RealTimeStatistics realTimeStatistics = new RealTimeStatistics();
 
-    
-    //
     public static void main(String[] args) throws InterruptedException
     {
-        int numRequestsPerCycle = ids.size();
-        int platformThreadsUsed = (PLATFORM_THREAD_USED != 0) ? PLATFORM_THREAD_USED  
-                : Runtime.getRuntime().availableProcessors();
-        
-        long minimumTimeBetweenRequestsInNs = (args.length > 0 ? 
-                Long.parseLong(args[0]) : DEFAULT_MINIMUM_TIME_BETWEEN_REQUESTS_IN_MICROSECONDS)*1000;
+        // Delete previous server statistics
+        ServerStatistics.delete();
 
-        System.setProperty("jdk.virtualThreadScheduler.maxPoolSize", 
+        // Set test parameters based on command-line arguments
+        setTestParameters(args);
+
+        // Prompt the user to press Enter to continue
+        pressEnterToContinue();
+
+        // Set the maximum pool size for virtual threads
+        System.setProperty("jdk.virtualThreadScheduler.parallelism",
+                Integer.toString(platformThreadsUsed));
+        System.setProperty("jdk.virtualThreadScheduler.maxPoolSize",
                 Integer.toString(platformThreadsUsed));
 
-        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
-        CountDownLatch latch = new CountDownLatch(numRequestsPerCycle * CYCLE_NUMBER);
+        // Create an executor service with virtual threads
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor())
+        {
+            // Initialize a latch to wait for completion of all tasks
+            CountDownLatch latch = new CountDownLatch(numRequestsPerCycle * Constants.CYCLE_NUMBER);
 
-        long startTime = System.currentTimeMillis();
+            // Record the start time of the test
+            long startTime = System.currentTimeMillis();
 
-        for (int cycleNumber = 0; cycleNumber < CYCLE_NUMBER; cycleNumber++)
+            // Submit a task to make concurrent requests as virtual thread
+            // => this task does not have its own platform thread
+            executor.submit(() -> concurrentRequests(executor, latch));
+
+            // Wait for all requests to complete or timeout
+            if (!latch.await(Constants.MAXIMUM_TIME_ALL_THREADS, TimeUnit.MILLISECONDS))
+            {
+                System.out.println("! Timeout");
+            }
+
+            // Calculate total time taken for the test
+            long totalTime = System.currentTimeMillis() - startTime;
+            // Print the results of the test
+            printResults(totalTime);
+
+        }
+    }
+
+    private static void setTestParameters(String[] args)
+    {
+        //
+        numRequestsPerCycle = Constants.ids.size();
+        platformThreadsUsed = (Constants.PLATFORM_THREADS_USED != 0) ? Constants.PLATFORM_THREADS_USED
+                : Runtime.getRuntime().availableProcessors();
+
+        minimumConcurrentRequest = Constants.MINIMUM_CONCURRENT_REQUEST;
+        maximumConcurrentRequest = Constants.MAXIMUM_CONCURRENT_REQUEST;
+        minimumTimeBetweenRequestsInNs = Constants.DEFAULT_TIME_BETWEEN_REQUESTS_IN_MICROSECONDS * 1000;
+
+        if (args.length > 0)
+        {
+            if (args.length == 3)
+            {
+                try
+                {
+                    minimumConcurrentRequest = Long.parseLong(args[0]);
+                    minimumTimeBetweenRequestsInNs = Long.parseLong(args[1]) * 1000;
+                    maximumConcurrentRequest = Long.parseLong(args[2]);
+                } catch (NumberFormatException e)
+                {
+                    System.out.println("Invalid parameter format. Please provide numeric values.");
+                    System.exit(1); // Terminate the program
+                }
+            } else
+            {
+                System.out.println("java -jar stressor.jar minimumConcurrentRequest delayBetweenRequestAfterThat maximumConcurrentRequest");
+                System.exit(1); // Terminate the program
+            }
+        }
+    }
+
+    private static void virtualThreadSwitchAndDelay()
+    {
+        try
+        {
+            if (concurrentRequest.get() > minimumConcurrentRequest)
+            {
+                if (concurrentRequest.get() > maximumConcurrentRequest)
+                {
+                    Thread.sleep(Constants.TIME_BETWEEN_REQUESTS_AFTER_MAXIMUM_CONCURRENT_REQUEST);
+                } else
+                {
+                    long requestTimeInNs = System.nanoTime();
+                    if ((requestTimeInNs - previousRequestTimeInNs) < minimumTimeBetweenRequestsInNs)
+                    {
+                        long nanos = (minimumTimeBetweenRequestsInNs - (requestTimeInNs - previousRequestTimeInNs));
+
+                        Thread.sleep((nanos / 1_000_000), (int) (nanos % 1_000_000));
+                    }
+                }
+            } else
+            {
+                Thread.sleep(0, Constants.TIME_BETWEEN_REQUESTS_BEFORE_MINIMUM_CONCURRENT_REQUEST_IN_NS);
+            }
+            previousRequestTimeInNs = System.nanoTime();
+
+        } catch (InterruptedException e)
+        {
+        }
+
+    }
+
+    public static void pressEnterToContinue()
+    {
+        //
+        System.out.println("java -jar stressor.jar "
+                + minimumConcurrentRequest + " " + minimumTimeBetweenRequestsInNs / 1000L
+                + " " + maximumConcurrentRequest);
+
+        System.out.println("Press Enter to continue...");
+        try(Scanner scanner = new Scanner(System.in))
+        {
+            scanner.nextLine();
+        }
+   
+    }
+
+    /**
+     * Makes concurrent requests using virtual threads.
+     *
+     * @param executor The executor service for running tasks
+     * @param latch The count down latch to synchronize threads
+     */
+    public static void concurrentRequests(ExecutorService executor, CountDownLatch latch)
+    {
+        for (int cycleNumber = 0; cycleNumber < Constants.CYCLE_NUMBER; cycleNumber++)
         {
             for (int cycleRequestNumber = 0; cycleRequestNumber < numRequestsPerCycle; cycleRequestNumber++)
             {
-                final int finalCycleRequestNumber = cycleRequestNumber;
-                final int counter = cycleNumber * numRequestsPerCycle + cycleRequestNumber + 1;
+                final int threadCycleRequestNumber = cycleRequestNumber;
+                final int threadRequestNumber = cycleNumber * numRequestsPerCycle + cycleRequestNumber + 1;
 
-                long requestTimeInNs = System.nanoTime();
-                if ((requestTimeInNs - previousRequestTimeInNs) < minimumTimeBetweenRequestsInNs)
-                {
-                    delayedRequests++;
-                    long nanos = (minimumTimeBetweenRequestsInNs - (requestTimeInNs - previousRequestTimeInNs));
-                    
-                    Thread.sleep((nanos / 1_000_000), (int)(nanos%1_000_000));
-                }
-                previousRequestTimeInNs = System.nanoTime();
+                // Introduce configurable delay between requests ---------------
+                virtualThreadSwitchAndDelay();
 
-                executor.submit(() -> makeRequest(counter, ids.get(finalCycleRequestNumber), latch));
+                // asynchronous request ----------------------------------------               
+                long concurrent = concurrentRequest.incrementAndGet();
+                maximumConcurrentRequestAchieve.updateAndGet(current -> Math.max(current, concurrent));
+                System.out.println("Request: " + threadRequestNumber
+                        + " - Concurrent requests: " + concurrent);
+
+                executor.submit(
+                        () -> makeRequestWith3Retry(threadRequestNumber,
+                                Constants.ids.get(threadCycleRequestNumber), latch)
+                );
             }
         }
 
-        // Wait for all requests to complete
-        if (latch.await(MAXIMUM_TIME_ALL_THREADS, TimeUnit.MILLISECONDS) != true)
-        {
-            System.out.println("Timeout");
-        }
-
-        long endTime = System.currentTimeMillis();
-        long totalTime = endTime - startTime;
-
-        System.out.println("\nRESULTS --------------------------------------------");
-        
-//
-        long requestNumber = CYCLE_NUMBER * numRequestsPerCycle;
-        long requestPerSecond = (long)(CYCLE_NUMBER * numRequestsPerCycle*1000L) / totalTime;
-        double averageConcurrentRequests = (realTimeStatistics.calculateMean() * requestNumber)/ totalTime;        
-        
-        System.out.println("Request per second: " + requestPerSecond);        
-        System.out.println("Average of concurrent requests: " + averageConcurrentRequests);
-        System.out.println("Total time of all requests: " + totalTime + " ms");
-        //
-        System.out.println("Minimum time requests: " + minimum + " ms");
-        System.out.println("Maximum time requests: " + maximum + " ms");
-        System.out.println("Mean: " + 
-                String.format("%.2f", realTimeStatistics.calculateMean()) + " ms");        
-        System.out.println("Standard Deviation: " + 
-                String.format("%.2f",realTimeStatistics.calculateStandardDeviation()) + " ms");
-        //
-        System.out.println("Delayed Requests: " + delayedRequests);
-        System.out.println("Requests with exception: " + exception);
-        System.out.println("Requests with error: " + error);
-
-        System.out.println("\nCLIENT SETTINGS -------------------------------------------");
-        System.out.println("Platform threads used: " + platformThreadsUsed);
-        System.out.println("Minimum time between request: " + minimumTimeBetweenRequestsInNs/1000 + " µs");
-        System.out.println("Request Number: " + requestNumber);
-
-        executor.shutdown();
     }
 
-    private static void makeRequest(int requestNumber, int id, CountDownLatch latch) 
+    private static void makeRequestWith3Retry(int requestNumber, int id, CountDownLatch latch)
+    {
+        try
+        {
+            int retries = 0;
+            while (retries < Constants.REQUEST_RETRIES_NUMBER)
+            {
+                try
+                {                    
+                    makeRequest(requestNumber, id);
+                    break;
+                } catch (IOException e)
+                {
+                    exceptionRequestRetries.incrementAndGet();
+                    retries++;
+                    try
+                    {
+                        TimeUnit.MILLISECONDS.sleep(Constants.TIME_BETWEEN_RETRIES*retries);
+                    } catch (InterruptedException ignored)
+                    {
+                    }
+
+                    System.out.println("! - Response " + requestNumber + ": Exception: " + e.getMessage() + " - retry: " + retries);
+                    //e.printStackTrace();        
+                }
+            }
+            if (retries >= Constants.REQUEST_RETRIES_NUMBER)
+            {
+                System.out.println("! - Response " + requestNumber + " MAXIMUM OF RETRIES !!! ");
+                exception.getAndIncrement();
+            }
+        } catch (URISyntaxException e)
+        {
+            System.out.println("! - Response " + requestNumber + ": Exception: " + e.getMessage());
+        } finally
+        {
+            latch.countDown();
+            concurrentRequest.getAndDecrement();
+        }
+    }
+
+    private static void makeRequest(int requestNumber, int id) throws URISyntaxException, MalformedURLException, IOException
     {
         HttpURLConnection connection = null;
         try
         {
-            URL url = new URI(BASE_URL+id).toURL();
+            URL url = new URI(Constants.BASE_URL + id).toURL();
 
             connection = (HttpURLConnection) url.openConnection();
 
-            long startTime = System.currentTimeMillis();
+            connection.setConnectTimeout(Constants.MAXIMUM_TIME_PER_REQUEST_CONNECTION);
+            connection.setReadTimeout(Constants.MAXIMUM_TIME_PER_REQUEST_READ);
+
+            long startTime = System.nanoTime();
 
             connection.setRequestMethod("GET");
 
@@ -142,33 +254,83 @@ public class HTTPServerParallelStressTest
 
             if (responseCode == HttpURLConnection.HTTP_OK)
             {
-                long endTime = System.currentTimeMillis();
+                long endTime = System.nanoTime();
                 long requestTime = endTime - startTime;
-                minimum.updateAndGet(current -> Math.min(current, requestTime));
-                maximum.updateAndGet(current -> Math.max(current, requestTime));
                 realTimeStatistics.addValue(requestTime);
 
                 long localResponseNumber = responseNumber.incrementAndGet();
                 String difference = (localResponseNumber == requestNumber) ? "= " : "* ";
 
                 System.out.println(difference + localResponseNumber
-                        + " - Request " + requestNumber + ": OK, in " + requestTime + "ms");
+                        + " - Response " + requestNumber + ": OK, in "
+                        + String.format("%.2f", (double) requestTime / 1000_000.00)
+                        + " ms");
             } else
             {
                 error.incrementAndGet();
-                System.out.println("Request " + requestNumber + ": Error - Response Code: " + responseCode);
+                System.out.println("! - Response " + requestNumber + ": Error Code: " + responseCode);
             }
-
-            connection.disconnect();
-        } catch (IOException | URISyntaxException e)
-        {
-            exception.getAndIncrement();
-            System.out.println("Request " + requestNumber + ": Error - Exception: " + e.getMessage());
-
         } finally
         {
-            if (connection != null) connection.disconnect();
-            latch.countDown();
+            if (connection != null)
+            {
+                connection.disconnect();
+            }
         }
     }
+
+    static private void printResults(long totalTime)
+    {
+
+        System.out.println("\nRESULTS -----------------------------------------------------");
+
+//
+        long requestNumber = Constants.CYCLE_NUMBER * numRequestsPerCycle;
+        long requestPerSecond = (long) (Constants.CYCLE_NUMBER * numRequestsPerCycle * 1000L) / totalTime;
+        double averageConcurrentRequests = (realTimeStatistics.calculateMean() * requestNumber) / (totalTime * 1000_000L);
+
+        System.out.println("Request per second: " + requestPerSecond);
+        System.out.println("Maximum concurrent request: " + maximumConcurrentRequestAchieve);
+        System.out.println("Average of concurrent requests: "
+                + String.format("%.2f", averageConcurrentRequests));
+        System.out.println("Total time of all requests: " + totalTime + " ms");
+        //        
+        System.out.println("Minimum time requests: "
+                + String.format("%.2f", (double) (realTimeStatistics.getMinimumRequestTime()) / 1000_000.00) + " ms");
+        System.out.println("Maximum time requests: "
+                + String.format("%.2f", (double) realTimeStatistics.getMaximumRequestTime() / 1000_000.00) + " ms");
+        System.out.println("Mean: "
+                + String.format("%.2f", realTimeStatistics.calculateMean() / 1000_000.00) + " ms");
+        System.out.println("Standard Deviation: "
+                + String.format("%.2f", realTimeStatistics.calculateStandardDeviation() / 1000_000.00) + " ms");
+        //
+        System.out.println("Requests with exception retries: " + exceptionRequestRetries);
+        System.out.println("Requests with exception after retrying: " + exception);
+        //System.out.println("Requests with no IO exception: " + noIOexception);
+        System.out.println("Requests with error: " + error);
+
+        System.out.println("\nCLIENT SETTINGS ---------------------------------------------");
+        System.out.println("Platform threads used: " + platformThreadsUsed);
+        System.out.println("Until " + minimumConcurrentRequest
+                + " concurrent request, new request each "
+                + Constants.TIME_BETWEEN_REQUESTS_BEFORE_MINIMUM_CONCURRENT_REQUEST_IN_NS + " ns.");
+        System.out.println("From " + minimumConcurrentRequest + " to " + maximumConcurrentRequest
+                + " concurrent requests, new requests each "
+                + minimumTimeBetweenRequestsInNs / 1000L + " µs");
+        System.out.println("After " + maximumConcurrentRequest
+                + " concurrent requests, new requests each "
+                + Constants.TIME_BETWEEN_REQUESTS_AFTER_MAXIMUM_CONCURRENT_REQUEST + " ms");
+        System.out.println("Total Request Number: " + requestNumber);
+
+        // print server statistics 
+        ServerStatistics.get();
+
+        System.out.println("\nEQUIVALENT CALL ---------------------------------------------");
+        System.out.println("java -jar stressor.jar "
+                + minimumConcurrentRequest + " " + minimumTimeBetweenRequestsInNs / 1000L
+                + " " + maximumConcurrentRequest);
+
+        System.out.println("\nThis is the end. --------------------------------------------\n ");
+    }
+
 }
